d7bff5b169b6276f728ba257de5ee7c398f455c7
wwx <2120405615@qq.com>
2024-09-20 21:05:46 +0800

    no message

--- Assets/_Scripts/Player/PlayerMiddle.cs
+++ Assets/_Scripts/Player/PlayerMiddle.cs
@@ -2,25 +2,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerMiddle : Singleton<PlayerMiddle>
 {
     public Transform[] playerTransform;
     private int playerIndex=0;
     private Vector3 middlePosition;
+
+    public CinemachineVirtualCamera virtualCamera;
+    public float maxSize;
+    public float mainSize;
     protected override void Awake()
     {
         base.Awake();
+        ChangeCameraSize();
     }
 
     protected override void OnDestroy()
     {
         base.OnDestroy();
     }
+    private void Update()
+    {
+        ChangeCameraSize();
+    }
     private void LateUpdate()
     {
         getMiddle();
         transform.position = middlePosition;
+        
     }
 
     public void GetPlayer(Transform transform)
@@ -44,4 +55,11 @@ public class PlayerMiddle : Singleton<PlayerMiddle>
             middlePosition = (playerTransform[0].position+playerTransform[1].position)/2;
         }
     }
+
+    public void ChangeCameraSize()
+    {
+        
+        virtualCamera.m_Lens.OrthographicSize += Mathf.Lerp(0,5,0.1f);
+    }
+
 }
