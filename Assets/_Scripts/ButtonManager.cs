a2aeafe81dcec558212408a9e9367cad7c1cd159
Daimaojin <1127677441@qq.com>
2024-09-21 23:55:31 +0800

    WallJump

--- Assets/_Scripts/ButtonManager.cs
+++ Assets/_Scripts/ButtonManager.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
 {
-    public GameObject DoorOpen;
-    public GameObject DoorClose;
+    private bool isDoorOpen = false;
+
+    public Transform Pos_DoorClose;
+    public Transform Pos_DoorOpen;
+
+    public Transform target_DoorPos;
+
+    public float moveSpeed = 5f;
+
+    public Transform Pos_ButtonOff;
+    public Transform Pos_buttonOn;
+
+    public Transform target_buttonPos;
+
+    public GameObject Door;
+    public GameObject Button;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target_DoorPos = Pos_DoorClose;
+        target_buttonPos = Pos_ButtonOff;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -17,8 +33,7 @@ public class ButtonManager : MonoBehaviour
         {
             if(collision.gameObject.CompareTag("Player")|| collision.gameObject.CompareTag("Snow"))
             {
-                DoorClose.SetActive(false);
-                DoorOpen.SetActive(true);
+                isDoorOpen = true;
             }
         }
     }
@@ -29,14 +44,25 @@ public class ButtonManager : MonoBehaviour
         {
             if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Snow"))
             {
-                DoorClose.SetActive(true);
-                DoorOpen.SetActive(false);
+               isDoorOpen= false;
             }
         }
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if(isDoorOpen)
+        {
+            target_DoorPos = Pos_DoorOpen;
+            target_buttonPos = Pos_buttonOn;
+        }else
+        {
+            target_DoorPos = Pos_DoorClose;
+            target_buttonPos = Pos_ButtonOff;
+        }
+
+        Door.transform.position = Vector2.MoveTowards(Door.transform.position, target_DoorPos.position, moveSpeed * Time.deltaTime);
+        Button.transform.position = Vector2.MoveTowards(transform.position, target_buttonPos.position, moveSpeed * Time.deltaTime);
     }
+
 }
